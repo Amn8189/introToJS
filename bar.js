123ae90948bb7d204fda9a799d05ef81f4b5e01e
Amn8189 <aydenmunga@gmail.com>
2025-04-16 19:31:52 +0300

    quiz.html

---
+++ bar.js
@@ -0,0 +1,10 @@
+//bar.js
+function sayHello() {
+    console.log("Hello");
+}
+
+function sayGoodbye() {
+    console.log("Bye");
+}
+
+export {sayHello, sayGoodbye};
---
+++ foo.js
@@ -0,0 +1,5 @@
+//foo.js
+import {sayGoodbye, sayHello} from "./bar.js"
+
+sayHello();
+sayGoodbye();
